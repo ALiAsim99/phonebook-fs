e53c99681614d3f474ef6367878ee778c7c5c14f
asim97-spec <ali.99asim@gmail.com>
2024-08-08 13:13:41 +0500

    removed wrong folder

--- backend/index.js
+++ backend/index.js
@@ -3,8 +3,8 @@ const app = express();
 const cors=require('cors')
 const PORT=process.env.PORT||3001
 const morgan=require('morgan')
-app.use(cors())
 app.use(express.json());
+app.use(express.static('dist'))
 morgan.token('body', (req) => {
     return JSON.stringify(req.body);
   });
